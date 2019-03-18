693e5abf5fa556f99b8af12f52f4e49e07fbe0a6
Luna <lucidlyluna@gmail.com>
2019-03-18 19:57:08 -0400

    Use Result instead of panics in parser

--- rsfg/src/parser.rs
+++ rsfg/src/parser.rs
@@ -2,12 +2,46 @@
 
 use crate::{Token, Type, ast::*};
 
-fn pop_no_eof(from: &mut Vec<Token>, parsing_what: &str) -> Token {
-	from.pop().expect(&format!("enexpected EOF parsing {}", parsing_what))
+#[derive(Debug)]
+enum ParseError {
+	Expected(String, String),
+	Unsupported(String),
+	EOF(String),
+	Other(String),
+}
+impl std::fmt::Display for ParseError {
+	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+		use ParseError::*;
+		match self {
+			Expected(what, after) => write!(f, "expected {} after {}", what, after),
+			Unsupported(what) => write!(f, "unsupported {}", what),
+			EOF(parsing) => write!(f, "unexpected EOF parsing {}", parsing),
+			Other(msg) => write!(f, "{}", msg),
+		}
+	}
+}
+// All relevant details in Display and Debug
+impl std::error::Error for ParseError {}
+
+type Result<T> = std::result::Result<T, ParseError>;
+
+fn pop_no_eof(from: &mut Vec<Token>, parsing_what: &str) -> Result<Token> {
+	match from.pop() {
+		Some(token) => Ok(token),
+		None => Err(ParseError::EOF(parsing_what.to_string()))
+	}
+}
+fn expect_token(rtokens: &mut Vec<Token>, what: Token, during: &str) -> Result<()> {
+	let next = pop_no_eof(rtokens, during)?;
+	if next == what {
+		Ok(())
+	} else {
+		Err(ParseError::Expected(format!("{:?}", what), during.to_string()))
+	}
 }
 
-fn parse_id(rtokens: &mut Vec<Token>, type_required: bool) -> TypedId {
-	let name = match pop_no_eof(rtokens, "identifier") {
+fn parse_id(rtokens: &mut Vec<Token>, type_required: bool) -> Result<TypedId> {
+	let name = match pop_no_eof(rtokens, "identifier")? {
 		Token::Identifier(name) => name,
 		_ => panic!("identifier wasn't identifier (compiler bug)"),
 	};
@@ -16,25 +50,24 @@ fn parse_id(rtokens: &mut Vec<Token>, type_required: bool) -> TypedId {
 			rtokens.pop();
 			let id_type = match rtokens.pop() {
 				Some(Token::Type(id_type)) => id_type,
-				_ => panic!("expected type after colon"),
+				_ => return Err(ParseError::Expected(String::from("type"), String::from("colon"))),
 			};
-			return TypedId {
+			return Ok(TypedId {
 				name,
 				id_type,
-			};
+			});
 		}
 		_ => if type_required {
-			panic!("type required and not given for {}", name);
+			return Err(ParseError::Expected(String::from("type"), name));
 		},
 	}
-	TypedId {
+	Ok(TypedId {
 		name,
 		id_type: Type::Infer,
-	}
+	})
 }
 
-fn parse_expression(mut rtokens: &mut Vec<Token>) -> Result<Expression, &str> {
-	println!("WARNING: parse expression not implemented");
+fn parse_expression(rtokens: &mut Vec<Token>) -> Result<Expression> {
 	match rtokens.last() {
 		Some(Token::StringLit(_)) => {
 			if let Some(Token::StringLit(string)) = rtokens.pop() {
@@ -54,33 +87,26 @@ fn parse_expression(mut rtokens: &mut Vec<Token>) -> Result<Expression, &str> {
 				}));
 			}
 		},
-		_ => return Err("other expressions unimplemented"),
+		_ => return Err(ParseError::Unsupported("expression".to_string())),
 	}
 	unreachable!();
 }
 
-fn parse_call(mut rtokens: &mut Vec<Token>) -> Result<FnCall, &str> {
+fn parse_call(rtokens: &mut Vec<Token>) -> Result<FnCall> {
 	let name = match rtokens.pop() {
 		Some(Token::Identifier(name)) => name,
-		_ => return Err("expected identifier in function call"),
+		_ => return Err(ParseError::Expected("identifier".to_string(), "fn call".to_string())),
 	};
 	// Arguments
-	match pop_no_eof(rtokens, "fn call") {
-		Token::LParen => (),
-		_ => panic!("expected ("),
-	}
+	expect_token(rtokens, Token::LParen, "fn call")?;
 	let mut arguments = vec![];
 	loop {
 		// TODO: is allowing commas at the beginning the worst idea ive had?
-		let result = match rtokens.last() {
+		arguments.push(match rtokens.last() {
 			Some(Token::RParen) => { rtokens.pop(); break },
 			Some(Token::Comma) => { rtokens.pop(); continue },
 			_ => parse_expression(rtokens),
-		};
-		match result {
-			Ok(expr) => arguments.push(expr),
-			Err(err) => panic!(format!("{}", err)),
-		}
+		}?);
 	}
 	Ok(FnCall {
 		name,
@@ -88,116 +114,107 @@ fn parse_call(mut rtokens: &mut Vec<Token>) -> Result<FnCall, &str> {
 	})
 }
 
-fn parse_args(mut rtokens: &mut Vec<Token>) -> Vec<TypedId> {
+fn parse_args(rtokens: &mut Vec<Token>) -> Result<Vec<TypedId>> {
 	let mut args = vec![];
-	match pop_no_eof(rtokens, "parameters") {
-		Token::LParen => (),
-		_ => panic!("expected ("),
-	}
+	expect_token(rtokens, Token::LParen, "fn parameters")?;
 	loop {
 		args.push(match rtokens.last() {
-			Some(Token::Identifier(_)) => parse_id(&mut rtokens, true),
+			Some(Token::Identifier(_)) => parse_id(rtokens, true)?,
 			Some(Token::RParen) => { rtokens.pop(); break },
-			Some(_) => panic!("expected Identifier or RParen"),
-			None => panic!("unexpected EOF parsing parameters"),
+			Some(_) => return Err(ParseError::Expected("Identifier or RParen".to_string(), "LParen".to_string())),
+			None => return Err(ParseError::EOF("parameters".to_string())),
 		});
 		match rtokens.pop() {
 			Some(Token::Comma) => (),
 			Some(Token::RParen) => break,
-			_ => panic!("expected comma or RParen"),
+			_ => return Err(ParseError::Expected("comma or RParen".to_string(), "argument".to_string())),
 		}
 	}
-	args
+	Ok(args)
 }
 
-fn parse_statement(mut rtokens: &mut Vec<Token>) -> Statement {
-	println!("WARNING: assignment statement unimplemented");
-	match parse_call(&mut rtokens) {
-		Ok(call) => Statement::FnCall(call),
-		Err(err) => { println!("{}", err); panic!("^^^^^ this, bro") },
-	}
+fn parse_statement(rtokens: &mut Vec<Token>) -> Result<Statement> {
+	Ok(Statement::FnCall(parse_call(rtokens)?))
 }
 
-fn parse_signature(mut rtokens: &mut Vec<Token>) -> Signature {
-	let parameters = parse_args(&mut rtokens);
+fn parse_signature(rtokens: &mut Vec<Token>) -> Result<Signature> {
+	let parameters = parse_args(rtokens)?;
 	let return_type = match rtokens.last() {
 		Some(Token::Type(_)) => match rtokens.pop() {
 			Some(Token::Type(r_type)) => Some(r_type),
-			_ => panic!("shouldn't be reachable"),
+			_ => unreachable!(),
 		},
 		Some(Token::Newline) => None,
-		Some(_) => panic!("expected newline or return type"),
-		None => panic!("expected end of signature"),
+		Some(_) => return Err(ParseError::Expected("newline or return type".to_string(), "signature".to_string())),
+		None => return Err(ParseError::EOF("signature".to_string())),
 	};
 	match rtokens.pop() {
 		Some(Token::Newline) => (),
-		_ => panic!("expected newline after definition"),
+		_ => return Err(ParseError::Expected("newline".to_string(), "definition".to_string())),
 	}
-	Signature {
+	Ok(Signature {
 		parameters: parameters,
 		return_type: return_type,
-	}
+	})
 }
 
-fn parse_fn(mut rtokens: &mut Vec<Token>) -> Fn {
+fn parse_fn(rtokens: &mut Vec<Token>) -> Result<Fn> {
 	// Parse signature
 	match rtokens.pop() {
 		Some(Token::Fn) => (),
-		Some(_) => panic!("fn didn't start with fn"),
-		None => panic!("expected signature after fn"),
+		_ => panic!("internal: fn didn't start with fn"),
 	}
-	let name = match pop_no_eof(rtokens, "fn") {
+	let name = match pop_no_eof(rtokens, "fn")? {
 		Token::Identifier(name) => name,
-		_ => panic!("expected fn name"),
+		_ => return Err(ParseError::Expected("fn name".to_string(), "fn".to_string())),
 	};
-	let signature = parse_signature(&mut rtokens);
+	let signature = parse_signature(rtokens)?;
 	let mut statements = vec![];
 	loop {
 		let t = match rtokens.last() {
 			Some(t) => t,
-			None => return Fn {
+			None => return Ok(Fn {
 				name,
 				signature,
 				statements,
-			}
+			})
 		};
 		match t {
 			Token::Tab => { rtokens.pop(); },
 			_ => break,
 		}
-		statements.push(parse_statement(rtokens));
+		statements.push(parse_statement(rtokens)?);
 		match rtokens.pop() {
 			Some(Token::Newline) => (),
 			None => (),
-			Some(t) => panic!("expected newline after statement, got {:?}", t),
+			Some(_) => return Err(ParseError::Expected("newline".to_string(), "statement".to_string())),
 		}
 	}
-	Fn {
+	Ok(Fn {
 		name,
 		signature,
 		statements,
-	}
+	})
 }
 
-pub fn parse_extern_fn(mut rtokens: &mut Vec<Token>) -> ExternFn {
+fn parse_extern_fn(rtokens: &mut Vec<Token>) -> Result<ExternFn> {
 	match rtokens.pop() {
 		Some(Token::ExternFn) => (),
-		Some(_) => panic!("extern fn didn't start with @fn"),
-		None => panic!("expected signature after @fn"),
+		_ => panic!("internal: extern fn didn't start with @fn"),
 	}
 	// An extern function that serves only as a typecheck might use the
 	// @ in the name. The lexer misinterprets this as ExternFnCall despite
 	// not being a call
-	let name = match pop_no_eof(rtokens, "fn") {
+	let name = match pop_no_eof(rtokens, "fn")? {
 		Token::ExternFnCall(name) => name,
 		Token::Identifier(name) => name,
-		_ => panic!("expected name of function, with or without leading @"),
+		_ => return Err(ParseError::Other("expected name of function, with or without leading @".to_string())),
 	};
-	let signature = parse_signature(&mut rtokens);
-	ExternFn {
+	let signature = parse_signature(rtokens)?;
+	Ok(ExternFn {
 		name,
 		signature,
-	}
+	})
 }
 
 pub fn parse(mut tokens: Vec<Token>) -> AST {
@@ -215,10 +232,10 @@ pub fn parse(mut tokens: Vec<Token>) -> AST {
 		match t {
 			// Parse a function
 			Token::Fn => {
-				ast.push(ASTNode::Fn(parse_fn(&mut rtokens)));
+				ast.push(ASTNode::Fn(parse_fn(&mut rtokens).unwrap()));
 			},
 			Token::ExternFn => {
-				ast.push(ASTNode::ExternFn(parse_extern_fn(&mut rtokens)));
+				ast.push(ASTNode::ExternFn(parse_extern_fn(&mut rtokens).unwrap()));
 			},
 			Token::Newline => {},
 			_ => {
