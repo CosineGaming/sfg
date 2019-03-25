838093f8feb5cdb87f21f6857c8f750456e14b02
Luna <lucidlyluna@gmail.com>
2019-03-25 02:32:51 -0400

    Move read functions out into a mod

--- rvmfg/src/lib.rs
+++ rvmfg/src/lib.rs
@@ -1,7 +1,36 @@
 mod sfg_std;
-#[macro_use]
+mod read;
 mod thread;
 
 pub use thread::Thread;
-pub use thread::Pushable;
+
+/// let mut thread = Thread::new(program);
+/// call![thread.main()]
+#[macro_export]
+macro_rules! call {
+	( $thread:ident.$function:ident($( $push:expr )*) ) => {
+		{
+			$(
+				$push.push_to(&mut $thread);
+			)*
+			$thread.call_name(stringify!($function));
+		}
+	};
+}
+
+/// This trait serves only to make the macro work easily
+/// relevant push_[type]()s and call_name() are just as effective
+pub trait Pushable {
+	fn push_to(&self, thread: &mut Thread);
+}
+impl Pushable for String {
+	fn push_to(&self, thread: &mut Thread) {
+		thread.push_string(self);
+	}
+}
+impl Pushable for str {
+	fn push_to(&self, thread: &mut Thread) {
+		thread.push_string(&self.to_string());
+	}
+}
 
--- rvmfg/src/thread.rs
+++ rvmfg/src/thread.rs
@@ -1,4 +1,5 @@
 use crate::sfg_std;
+use crate::read::*;
 
 use indexmap::IndexMap;
 
@@ -26,164 +27,31 @@ pub struct Thread {
 }
 
 #[derive(PartialEq, Clone, Debug)]
-struct Fn {
+pub struct Fn {
 	// ip will be 0 for externs (TODO: make this safer)
 	ip: u32,
 	return_type: Option<Type>,
 	parameters: Vec<Type>,
 }
-
-#[derive(PartialEq, Eq, Clone, Debug)]
-enum Type {
-	Str,
-	Int,
-}
-
-#[derive(PartialEq, Eq, Debug)]
-enum Deser {
-	Add,
-	Dup,
-	Equals,
-	ExternFnCall,
-	ExternFnHeader,
-	FnCall,
-	FnHeader,
-	Panic,
-	JumpZero,
-	Pop32,
-	Push32,
-	Return,
-	StringLit,
-	Sub,
-	Type(Type),
-	Void,
-}
-
-fn deser(what: u8) -> Option<Deser> {
-	use Deser as D;
-	use Type::*;
-	match what {
-		// Types 1x
-		0x10 => Some(D::Type(Int)),
-		0x11 => Some(D::Type(Str)),
-		// Other 2x
-		0x21 => Some(D::Void),
-		// Instructions 3x
-		0x30 => Some(D::Push32),
-		0x31 => Some(D::ExternFnCall),
-		0x32 => Some(D::StringLit),
-		0x33 => Some(D::FnHeader),
-		0x34 => Some(D::ExternFnHeader),
-		0x35 => Some(D::Return),
-		0x36 => Some(D::FnCall),
-		0x37 => Some(D::Pop32),
-		0x38 => Some(D::Equals),
-		0x39 => Some(D::JumpZero),
-		0x3a => Some(D::Dup),
-		0x3b => Some(D::Panic),
-		0x3c => Some(D::Add),
-		0x3d => Some(D::Sub),
-		_ => None,
-	}
-}
-fn deser_strong(what: u8) -> Deser {
-	deser(what).expect(&format!("tried to match invalid u8 0x{:X}", what))
-}
-
-fn expect(code: &Vec<u8>, mut ip: &mut usize, what: u8, message: &str) {
-	if next(&code, &mut ip) != what {
-		panic!("{}", message);
-	}
-}
-
-fn next(code: &Vec<u8>, ip: &mut usize) -> u8 {
-	let rv = code[*ip];
-	*ip += 1;
-	rv
-}
-
-fn read_u32(code: &Vec<u8>, ip: &mut usize) -> u32 {
-	use std::mem::transmute;
-	let mut four: [u8; 4] = Default::default();
-	four.copy_from_slice(&code[*ip..*ip+4]);
-	let rv = u32::from_le(unsafe { transmute::<[u8; 4], u32>(four) });
-	*ip += 4;
-	rv
-}
-fn read_i32(code: &Vec<u8>, ip: &mut usize) -> i32 {
-	use std::mem::transmute;
-	let mut four: [u8; 4] = Default::default();
-	four.copy_from_slice(&code[*ip..*ip+4]);
-	let rv = i32::from_le(unsafe { transmute::<[u8; 4], i32>(four) });
-	*ip += 4;
-	rv
-}
-
-fn read_to_zero(code: &Vec<u8>, mut ip: &mut usize) -> Vec<u8> {
-	let mut rv = Vec::new();
-	loop {
-		let b = next(code, &mut ip);
-		if b == 0 {
-			break;
+impl Fn {
+	pub fn new(ip: u32, return_type: Option<Type>, parameters: Vec<Type>) -> Self {
+		Self {
+			ip,
+			return_type,
+			parameters,
 		}
-		rv.push(b);
 	}
-	rv
-}
-
-fn read_string(code: &Vec<u8>, mut ip: &mut usize) -> String {
-	let bytes = read_to_zero(&code, &mut ip);
-	match String::from_utf8(bytes) {
-		Ok(string) => string,
-		Err(e) => panic!("invalid string {}", e),
-	}
-}
-
-/// Returns (name, function)
-fn read_fn_header(code: &Vec<u8>, mut ip: &mut usize, is_extern: bool) -> (String, Fn) {
-	let return_type_u8 = next(code, &mut ip);
-	let return_type = match deser_strong(return_type_u8) {
-		Deser::Type(t) => Some(t),
-		Deser::Void => None,
-		_ => panic!("expected type or void, got {}", return_type_u8),
-	};
-	let param_count = next(code, &mut ip);
-	let mut parameters = vec![];
-	for _ in 0..param_count {
-		let type_u8 = next(code, &mut ip);
-		let param = match deser_strong(type_u8) {
-			Deser::Type(t) => t,
-			_ => panic!("expected type, got {}", type_u8),
-		};
-		parameters.push(param);
-	}
-	let name = read_string(&code, &mut ip);
-	let codeloc = if is_extern {
-		0
-	} else {
-		read_u32(code, &mut ip)
-	};
-	let func = Fn {
-		return_type,
-		parameters,
-		ip: codeloc,
-	};
-	(name, func)
-}
-
-// This function may be a little redundant, but I wanna keep it for a bit
-// in case StringLit gets more complex
-fn read_string_lit(code: &Vec<u8>, mut ip: &mut usize) -> String {
-	read_string(&code, &mut ip)
 }
 
 impl Thread {
 	pub fn new(code: Vec<u8>) -> Self {
 		let mut ip = 0;
-		expect(&code, &mut ip, 'b' as u8, "expected bcfg");
-		expect(&code, &mut ip, 'c' as u8, "expected bcfg");
-		expect(&code, &mut ip, 'f' as u8, "expected bcfg");
-		expect(&code, &mut ip, 'g' as u8, "expected bcfg");
+		if next(&code, &mut ip) != 'b' as u8 ||
+			next(&code, &mut ip) != 'c' as u8 ||
+			next(&code, &mut ip) != 'f' as u8 ||
+			next(&code, &mut ip) != 'g' as u8 {
+			panic!("expected bcfg");
+		}
 		let mut fns = IndexMap::new();
 		let mut strings = Vec::new();
 		loop {
@@ -210,7 +78,7 @@ impl Thread {
 			match deser(code[ip]) {
 				Some(Deser::StringLit) => {
 					ip += 1;
-					let string = read_string_lit(&code, &mut ip);
+					let string = read_string(&code, &mut ip);
 					strings.push(string);
 				},
 				_ => break,
@@ -342,35 +210,6 @@ impl Thread {
 			None => panic!("could not find function {}", name),
 		};
 		self.call_fn(func);
-	}
-}
-
-/// call![thread.main()]
-#[macro_export]
-macro_rules! call {
-	( $thread:ident.$function:ident($( $push:expr )*) ) => {
-		{
-			$(
-				$push.push_to(&mut $thread);
-			)*
-			$thread.call_name(stringify!($function));
-		}
-	};
-}
-
-/// This trait serves only to make the macro work easily
-/// relevant push_[type]()s and call_name() are just as effective
-pub trait Pushable {
-	fn push_to(&self, thread: &mut Thread);
-}
-impl Pushable for String {
-	fn push_to(&self, thread: &mut Thread) {
-		thread.push_string(self);
-	}
-}
-impl Pushable for str {
-	fn push_to(&self, thread: &mut Thread) {
-		thread.push_string(&self.to_string());
 	}
 }
 
